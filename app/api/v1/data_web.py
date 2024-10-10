0d6114e8790818bfd9808139673f18453099cd22
JuanMGlez <juanmglezrazo@gmail.com>
2024-10-10 11:25:33 -0600

    fixer

--- app/api/v1/data_web.py
+++ app/api/v1/data_web.py
@@ -1,9 +1,11 @@
 from app.services.sensor_data_batch import get_sensor_data
 from fastapi import APIRouter, Depends
+from app.models.devices import DeviceState
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.postgres import get_async_db
 from typing import Optional
 from fastapi import Query
+from sqlalchemy import select, func
 
 router = APIRouter()
 
@@ -17,3 +19,32 @@ async def get_sensor_data_batch(
 ):
     return await get_sensor_data(db, sensor_id, limit, offset)
 
+@router.get("/devices")
+async def get_sensor_data_batch(
+    db: AsyncSession = Depends(get_async_db)
+):
+    # Subconsulta para obtener el último timestamp de cada device_id
+    subquery = (
+        select(DeviceState.device_id, func.max(DeviceState.created_at).label("latest_timestamp"))
+        .group_by(DeviceState.device_id)
+        .subquery()
+    )
+
+    # Consulta principal para obtener el estado más reciente
+    query = (
+        select(DeviceState)
+        .join(subquery,
+              (DeviceState.device_id == subquery.c.device_id) & (DeviceState.created_at == subquery.c.latest_timestamp))
+    )
+
+    result = await db.execute(query)
+    device_states = result.scalars().all()
+
+    # Estructurar la respuesta en una lista de diccionarios
+    return [
+        {
+            "device_id": ds.device_id,
+            "state": ds.state,
+            "timestamp": ds.created_at
+        } for ds in device_states
+    ]
--- app/models/devices.py
+++ app/models/devices.py
@@ -1,6 +1,8 @@
 # app/models/devices.py
 from sqlalchemy import Column, Integer, String, Text
 from app.db.postgres import Base
+from sqlalchemy.sql import func
+from sqlalchemy.dialects.postgresql import TIMESTAMP
 
 class DeviceState(Base):
     __tablename__ = "device_states"
@@ -9,3 +11,5 @@ class DeviceState(Base):
     device_id = Column(Integer, nullable=False)
     state = Column(String, nullable=False)
     description = Column(Text, nullable=True)
+    created_at = Column(TIMESTAMP, server_default=func.now(), index=True)
+
